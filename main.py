21512e5a7c77b0b9c78df7ae01835604fe9cf28d
ma2za <mazzapaolo2019@gmail.com>
2023-10-04 10:30:41 +0200

    🦧 better asyncio

--- main.py
+++ main.py
@@ -4,46 +4,66 @@ import os.path
 from urllib.parse import urlparse, urljoin
 
 import httpx
+import yaml
 from bs4 import BeautifulSoup
 from redis.asyncio import Redis
 
 
+async def fetch(r, career_keywords):
+    url = await r.lpop("frontier")
+    try:
+        async with httpx.AsyncClient() as client:
+            resp = await client.get(url)
+    except Exception as e:
+        await r.rpush("frontier", url)
+        return url, str(e)
+    await r.lpush("visited", url)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+
+    for link in soup.find_all('a'):
+        new_url = link.get('href')
+        if not urlparse(new_url).netloc:
+            new_url = urljoin(url, new_url)
+        result = urlparse(new_url)
+        if not all([result.scheme, result.netloc]):
+            continue
+        if any([car in new_url for car in career_keywords]):
+            print(new_url)
+            await r.rpush("career_urls", new_url)
+            continue
+
+        out = await r.lpos("visited", new_url)
+
+        if out is None:
+            await r.rpush("frontier", new_url)
+
+    return url, "Done"
+
+
 async def main():
-    r = await Redis(host='redis', port=6379, decode_responses=True)
+    with open("data/config.yml", "r") as f:
+        config = yaml.load(f, yaml.Loader)
+        career_keywords = config.get("career_keywords")
+
+    r = await Redis(host='localhost', port=6379, decode_responses=True)
     urls = []
-    # await r.lpop(100000000)
-    if os.path.exists("data/urls.json"):
-        with open("data/urls.json", "r") as f:
+    await r.lpop("frontier", 100000000)
+    await r.lpop("visited", 100000000)
+
+    URLS = "data/temp.json"
+    if os.path.exists(URLS):
+        with open(URLS, "r") as f:
             urls = json.load(f)
-        os.rename("data/urls.json", "data/old_urls.json")
+        os.rename(URLS, "data/old_urls.json")
     if urls:
         await r.lpush("frontier", *urls)
-
+    i = 0
     while True:
-        url = await r.lpop("frontier")
-        print(url)
-        try:
-            async with httpx.AsyncClient() as client:
-                resp = await client.get(url)
-        except Exception as e:
-            print(e)
-            await r.rpush("frontier", url)
-            continue
-        await r.lpush("visited", url)
-        soup = BeautifulSoup(resp.text, 'html.parser')
-
-        for link in soup.find_all('a'):
-            new_url = link.get('href')
-            if not urlparse(new_url).netloc:
-                new_url = urljoin(url, new_url)
-            result = urlparse(new_url)
-            if not all([result.scheme, result.netloc]):
-                continue
-
-            out = await r.lpos("visited", new_url)
-            if out is None:
-                print(new_url)
-                await r.rpush("frontier", new_url)
+        tasks = [fetch(r, career_keywords) for _ in range(100)]
+        responses = await asyncio.gather(*tasks)
+        for url, response in responses:
+            i += 1
+            print(f"Response {i} from {url}: {response}")
 
 
 if __name__ == "__main__":
