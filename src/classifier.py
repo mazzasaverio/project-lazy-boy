33f92d68234d4f361646e551959a2bfde77ed2c8
ma2za <mazzapaolo2019@gmail.com>
2023-10-22 12:05:14 +0200

    🗽 cleanup

--- src/classifier.py
+++ src/classifier.py
@@ -3,6 +3,7 @@ import os
 
 import torch
 from dotenv import load_dotenv
+from memory_profiler import profile
 from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification
 
 logger = logging.getLogger(__name__)
@@ -12,27 +13,31 @@ load_dotenv()
 MODEL_ID = os.getenv("MODEL_ID")
 HF_CACHE = "../data"
 
-model_config = AutoConfig.from_pretrained(
-    MODEL_ID, use_auth_token=os.getenv("HF_TOKEN"), cache_dir=HF_CACHE
-)
 
-model = AutoModelForSequenceClassification.from_pretrained(
-    MODEL_ID,
-    config=model_config,
-    cache_dir=HF_CACHE,
-    use_safetensors=False,
-    low_cpu_mem_usage=True,
-)
+@profile()
+def load_model():
+    model_config = AutoConfig.from_pretrained(
+        MODEL_ID, use_auth_token=os.getenv("HF_TOKEN"), cache_dir=HF_CACHE
+    )
 
-tokenizer = AutoTokenizer.from_pretrained(
-    MODEL_ID,
-    cache_dir=HF_CACHE,
-)
+    model = AutoModelForSequenceClassification.from_pretrained(
+        MODEL_ID,
+        config=model_config,
+        cache_dir=HF_CACHE,
+        use_safetensors=False,
+        low_cpu_mem_usage=True,
+    )
 
-model.eval()
+    tokenizer = AutoTokenizer.from_pretrained(
+        MODEL_ID,
+        cache_dir=HF_CACHE,
+    )
+    return model, tokenizer
 
 
+@profile
 def local_llm(urls):
+    model.eval()
     inputs = tokenizer.batch_encode_plus(
         urls, return_tensors="pt", padding=True, truncation=True
     )
@@ -40,6 +45,8 @@ def local_llm(urls):
         outputs = model(**inputs)
     return outputs[0].argmax(dim=1).tolist()
 
+
+model, tokenizer = load_model()
 
 if __name__ == "__main__":
     print(local_llm(["http://rsga4u.com/apply.html", "http://rsga4u.com"]))
